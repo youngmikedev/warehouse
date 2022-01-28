c75ad83724fca1b19062e73a2bb6720019c80766
Young Mike <youngmikedev@gmail.com>
2022-01-28 14:39:39 -0500

    add repository user interface

---
+++ internal/repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/imranzahaev/warehouse/internal/domain"
+)
+
+type User interface {
+	Create(ctx context.Context, user domain.User, pwd string) error
+	Update(ctx context.Context, user domain.User, pwd string) error
+	Get(ctx context.Context, id int) (domain.User, error)
+	SetSession(ctx context.Context, session domain.Session) error
+	GetSession(ctx context.Context, userID int, refreshToken string) (*domain.Session, error)
+}
