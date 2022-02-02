fc329970fe33c30354f8fceb30cc20e18f65991a
Young Mike <youngmikedev@gmail.com>
2022-02-01 19:04:33 -0500

    upd get repository user interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -13,8 +13,8 @@ type Repositories struct {
 type User interface {
 	Create(ctx context.Context, user domain.User, password string) (int, error)
 	Update(ctx context.Context, user domain.User, password string) error
-	Get(ctx context.Context, id int) (domain.User, error)
-	GetByLogin(ctx context.Context, login string) (domain.User, error)
+	Get(ctx context.Context, id int) (u domain.User, password string, err error)
+	GetByLogin(ctx context.Context, login string) (u domain.User, password string, err error)
 	CreateSession(ctx context.Context, session domain.Session) (int, error)
 	UpdateSession(ctx context.Context, session domain.Session) error
 	GetSessionByAccess(ctx context.Context, token string) (domain.Session, error)
