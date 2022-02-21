1438ca55f388b2acd62507c83a72d72bc63e11a8
Young Mike <youngmikedev@gmail.com>
2022-02-21 20:43:19 +0300

    upd postgres client constuctor

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -1,8 +1,11 @@
 package postgres
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 
+	entsql "entgo.io/ent/dialect/sql"
 	"github.com/imranzahaev/warehouse/internal/repository/postgres/ent"
 
 	_ "github.com/lib/pq"
@@ -10,9 +13,23 @@ import (
 
 // NewClient established connection to a mongoDb instance using provided URI and auth credentials
 func NewClient(host, port, user, dbname, password string) (*ent.Client, error) {
-	client, err := ent.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
-		host, port, user, dbname, password))
+	databaseUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname)
+	sqlDB, err := sql.Open("postgres", databaseUrl)
 	if err != nil {
+		return nil, err
+	}
+
+	if err = sqlDB.Ping(); err != nil {
+		return nil, err
+	}
+
+	drv := entsql.OpenDB("postgres", sqlDB)
+	client := ent.NewClient(ent.Driver(drv))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Schema.Create(context.Background()); err != nil {
 		return nil, err
 	}
 
