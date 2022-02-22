dbe6c593c1a44f88aea593fac46ec99a26ccccc8
Young Mike <youngmikedev@gmail.com>
2022-02-22 21:30:15 +0300

    add app and main.go

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/imranzahaev/warehouse/internal/app"
+
+func main() {
+	app.Run()
+}
---
+++ internal/app/app.go
@@ -0,0 +1,92 @@
+package app
+
+import (
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/imranzahaev/warehouse/internal/auth"
+	"github.com/imranzahaev/warehouse/internal/config"
+	"github.com/imranzahaev/warehouse/internal/delivery/swagger"
+	"github.com/imranzahaev/warehouse/internal/repository"
+	"github.com/imranzahaev/warehouse/internal/repository/postgres"
+	"github.com/imranzahaev/warehouse/internal/service"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+)
+
+func Run() {
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	if err := godotenv.Load(); err != nil && !os.IsNotExist(err) {
+		logger.Fatal().
+			Err(err).
+			Msg("Failed init .env file")
+		return
+	}
+
+	cfg, err := config.Init()
+	if err != nil {
+		logger.Fatal().
+			Err(err).
+			Msg("Failed init config")
+		return
+	}
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.Level(cfg.Log.Level))
+
+	// PostrgeSQL
+	postgresClient, err := postgres.NewClient(
+		cfg.DB.Host,
+		cfg.DB.Port,
+		cfg.DB.User,
+		cfg.DB.Name,
+		cfg.DB.Password,
+	)
+	if err != nil {
+		logger.Fatal().
+			Err(err).
+			Msg("Failed init Postgres client")
+		return
+	}
+
+	repos := repository.NewPostgresRepositories(postgresClient)
+
+	tokenManager := auth.NewTokenManager(cfg.Token.SigningKey, time.Duration(cfg.Token.UTExpiresAt), time.Duration(cfg.Token.URTExpiresAT))
+	hashManager := auth.NewHashManager()
+	services := service.NewServices(repos, tokenManager, hashManager, &logger)
+
+	l := logger.With().Str("from", "delivery").Logger()
+	httpServer, err := swagger.NewServer(services, cfg, &l)
+	if err != nil {
+		logger.Fatal().
+			Err(err).
+			Msg("Failed init Postgres client")
+		return
+	}
+
+	// Start server
+	go func() {
+		if err := httpServer.Serve(); err != nil && err != http.ErrServerClosed {
+			logger.Fatal().
+				Err(err).
+				Msg("Shutting down the http server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	if err := httpServer.Shutdown(); err != nil {
+		logger.Fatal().
+			Err(err).
+			Msg("Failed shutting down the http server")
+	}
+	logger.Info().Msg("Http server successful shutdown")
+
+}
