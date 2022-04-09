dfe6af28b1a220c63a48a9af934dddc3c283bedb
Young Mike <youngmikedev@gmail.com>
2022-04-09 14:58:07 +0300

    add domain get many products filter

--- internal/domain/product.go
+++ internal/domain/product.go
@@ -9,3 +9,34 @@ type Product struct {
 	Name      string
 	CreatedAt time.Time
 }
+
+type GetManyProductsFilter struct {
+	UID int
+
+	ArticleLike string
+	NameLike    string
+
+	PriceFrom int
+	PriceTo   int
+
+	Page  int
+	Limit int
+
+	// One of:
+	//  id
+	//  article
+	//  name
+	//  price
+	//  created
+	SortField string
+	// ASC or DESC
+	SortOrder string
+}
+
+type GetManyProductsResponse struct {
+	Products []Product
+	Page     int
+	Limit    int
+
+	Count int
+}
